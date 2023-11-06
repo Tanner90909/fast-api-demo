12eafdddc9fad37fa2cd972295b32a19130c639b
Tanner90909 <tmmcdaniel909@gmail.com>
2023-11-06 20:41:22 +0000

    testing

---
+++ tests/test_fastapi.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+import pytest
+import httpx
+import sys
+
+sys.path.insert(1, '/workspaces/fast-api-demo/')
+
+from main import app
+
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
+
+def test_read_item():
+    tres = 3
+    response = client.get("/items/" + str(tres))
+    assert response.status_code == 200
+    assert response.json() == {"item_id": 3}

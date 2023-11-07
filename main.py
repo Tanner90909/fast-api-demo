067326615ac849d16677949235f674c4f3b797ab
Tanner90909 <tmmcdaniel909@gmail.com>
2023-11-07 21:23:38 +0000

    stuff and things

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
 from fastapi import FastAPI
-from enum import Enum
+from models import Hero, Relationship, RelationshipType, Ability, AbilityType
+import database
+from sqlalchemy import select
 
 app = FastAPI()
 
-class ModelName(str, Enum):
-        alexnet = "alexnet"
-        resnet = "resnet"
-        lenet = "lenet"
-
 @app.get("/")
-async def root():
-    return {"msg": "Hello World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
+async def read_root():
+    return "hello world"
